11a6cd9c43d0ce904c9a37b0036985b3eb2dca7c
Lydie Pluvinage <lydiepluvinage@yahoo.fr>
2022-04-03 14:28:38 +0200

    Diets GET ok

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 import setupRoutes from './router';
 import { handleError } from './helpers/errors';
-import connection from './db-config.js';
 import 'dotenv/config';
 
-const server = express();
-const PORT = process.env.PORT || 3000;
+const server: express.Application = express();
+const PORT: number = Number(process.env.PORT) || 3000;
 
 // Allows express to read requests body
 server.use(express.json());
@@ -16,14 +15,6 @@ setupRoutes(server);
 // Error managment
 server.use(handleError);
 
-server.get('/', (req, res) => {
-  connection
-    .query('SELECT * FROM diets')
-    .then((data) => res.status(200).json(data.rows));
-});
-
 server.listen(PORT, () => {
   console.log('Server running at http://127.0.0.1:' + PORT);
 });
-
-module.exports = server;

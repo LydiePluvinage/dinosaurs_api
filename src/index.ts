faf4479be0dd556ce6d1a1d35f0cb04f65a2fd04
Lydie Pluvinage <lydiepluvinage@yahoo.fr>
2022-04-11 19:28:23 +0200

    dinosaurs cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import setupRoutes from './router';
 import { handleError } from './helpers/errors';
 import 'dotenv/config';
 
 const server: express.Application = express();
 const PORT: number = Number(process.env.PORT) || 3000;
+
+// utilise le package cors pour autoriser les appels extérieurs
+server.use(
+  cors({
+    origin: '*',
+  })
+);
 
 // Allows express to read requests body
 server.use(express.json());
